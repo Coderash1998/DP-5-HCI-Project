e221b578b82b458c6280b550f757ca1e00cace1e
Coderash1998 <47770728+Coderash1998@users.noreply.github.com>
2022-11-24 00:14:03 -0600

    Completed the null validation for friends chat

--- JavaScript/Friends.js
+++ JavaScript/Friends.js
@@ -89,3 +89,51 @@ function clickRejected3() {
     document.getElementById("clickText3").classList.add('text-danger');
 
 }
+
+var sendMessageButton1 = document.getElementById("messageButton1")
+sendMessageButton1.addEventListener("click", textValidation1);
+
+function textValidation1() {
+    let checkText1 = document.getElementById('message1').value;
+    if (checkText1===''){
+        $('#viewJane').modal('show');
+        $('#failureMessage').modal('show')
+    }
+    else{
+        $('#viewJane').modal('hide');
+        $('#successMessage').modal('show')
+    }
+}
+
+
+var sendMessageButton2 = document.getElementById("messageButton2")
+sendMessageButton2.addEventListener("click", textValidation2);
+
+function textValidation2() {
+    let checkText2 = document.getElementById('message2').value;
+    if (checkText2===''){
+        $('#viewKurt').modal('show');
+        $('#failureMessage').modal('show');
+    }
+    else{
+        $('#viewKurt').modal('hide');
+        $('#successMessage').modal('show');
+    }
+}
+
+
+
+var sendMessageButton3 = document.getElementById("messageButton3")
+sendMessageButton3.addEventListener("click", textValidation3);
+
+function textValidation3() {
+    let checkText3 = document.getElementById('message3').value;
+    if (checkText3===''){
+        $('#viewMorgan').modal('show');
+        $('#failureMessage').modal('show')
+    }
+    else{
+        $('#viewMorgan').modal('hide');
+        $('#successMessage').modal('show')
+    }
+}
