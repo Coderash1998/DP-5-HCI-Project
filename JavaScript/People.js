a97dedce3590dc126f24def2d2123949e38a95f5
Sid <sarangi.siddharth1996@gmail.com>
2022-12-01 19:22:16 -0600

    Working on Sorting People

--- JavaScript/People.js
+++ JavaScript/People.js
@@ -23,3 +23,29 @@ function getId(id){
     modalImg.src=getImage.src
     document.getElementById('modalTitle').innerHTML=getName;
 }
+
+function sortTime(){
+    var ul=document.getElementById("groups-list")
+    var list=ul.getElementsByTagName("LI");
+    for(let i=0;i<list.length;i++)
+        console.log(list[0]);
+}
+
+function sortName(){
+    console.log('sortNames')
+    var ul=document.getElementById("groups-list")
+    Array.from(ul.getElementsByTagName("LI")).sort((a,b)=>a.textContent.localeCompare(b.textContent))
+    .forEach(li=>ul.appendChild(li))
+
+    var ul2=document.getElementById("groups-list2")
+    Array.from(ul2.getElementsByTagName("LI")).sort((a,b)=>a.textContent.localeCompare(b.textContent))
+    .forEach(li=>ul2.appendChild(li))
+}
+
+function selectOptions(){
+    let item=document.getElementById("sorting_options").value;
+    if(item==="alphabetical")
+        sortName();
+    if(item==="active")
+        sortTime();
+}
